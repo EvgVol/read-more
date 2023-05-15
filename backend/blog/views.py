6b0b2fa5255a1f020d619b5377cbd58700d8dc3e
Evgeniy V <volochek93@yandex.ru>
2023-05-15 16:57:19 +0400

    post_detail

--- backend/blog/views.py
+++ backend/blog/views.py
@@ -1,16 +1,6 @@
 from django.shortcuts import render, get_object_or_404
-from django.views import generic
 
 from .models import Post
-
-
-# class BlogListView(generic.ListView):
-#     """Отображает страницу блога с 8 постами."""
-
-#     model = Post
-#     template_name = "blog/blog.html"
-#     paginate_by = 4
-
 
 
 def post_list(request):
@@ -18,6 +8,6 @@ def post_list(request):
     return render(request, 'blog/blog.html', {'posts': posts})
 
 
-def post_detail(request):
-    post = get_object_or_404(Post, id=id, status=Post.Status.PUBLISH)
+def post_detail(request, id):
+    post = get_object_or_404(Post, id=id, status=Post.Status.PUBLISHED)
     return render(request, 'blog/post/post_detail.html', {'post': post})
