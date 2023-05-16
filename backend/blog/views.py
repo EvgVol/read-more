1c152cf2717592076447107ced1573670350e99a
Evgeniy V <volochek93@yandex.ru>
2023-05-16 14:27:57 +0400

    Class-based representations have been rewritten

--- backend/blog/views.py
+++ backend/blog/views.py
@@ -1,22 +1,30 @@
-from django.shortcuts import render, get_object_or_404
-from django.core.paginator import Paginator
+from django.views.generic import ListView, DetailView
 
 from .models import Post
 
 
-def post_list(request):
-    post_list = Post.published.all()
-    paginator = Paginator(post_list, 3)
-    page_number = request.GET.get('page', 1)
-    posts = paginator.page(page_number)
-    return render(request, 'blog/blog.html', {'posts': posts})
+class PostListView(ListView):
+    """Представление списка постов."""
+
+    queryset = Post.published.all()
+    context_object_name = 'posts'
+    paginate_by = 3
+    template_name = 'blog/blog.html'
 
 
-def post_detail(request, year, month, day, post):
-    post = get_object_or_404(Post,
-                             status=Post.Status.PUBLISHED,
-                             slug=post,
-                             publish__year=year,
-                             publish__month=month,
-                             publish__day=day )
-    return render(request, 'blog/blog-detail.html', {'post': post})
+class PostDetailView(DetailView):
+    model = Post
+    context_object_name = 'post'
+    template_name = 'blog/blog-detail.html'
+    slug_field = 'slug'
+    slug_url_kwarg = 'post'
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        return queryset.filter(
+            status=Post.Status.PUBLISHED,
+            publish__year=self.kwargs['year'],
+            publish__month=self.kwargs['month'],
+            publish__day=self.kwargs['day'],
+            slug=self.kwargs['post']
+        )
