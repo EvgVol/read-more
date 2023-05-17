e20bf3e6d519a9f29384649a0a16aa12604c06f6
Evgeniy V <volochek93@yandex.ru>
2023-05-17 21:05:35 +0400

    add view similar_posts

--- backend/blog/views.py
+++ backend/blog/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import get_object_or_404, render
 from django.core.mail import send_mail
 from django.views.decorators.http import require_POST
 from django.core.paginator import Paginator, PageNotAnInteger, EmptyPage
+from django.db.models import Count
 
 from taggit.models import Tag
 
@@ -42,10 +43,19 @@ def post_detail(request, year, month, day, post):
                               publish__day=day)
     comments = post.comments.filter(active=True)
     form = CommentForm()
+    post_tags_ids = post.tags.values_list('id', flat=True)
+    similar_posts = Post.published.filter(
+        tags__in=post_tags_ids
+    ).exclude(id=post.id)
+    similar_posts = similar_posts.annotate(
+        same_tags=Count('tags')
+    ).order_by('-same_tags','-publish')[:4]
+    
     return render(request, 'blog/blog-detail.html',
                   {'post': post,
                    'comments': comments,
-                   'form': form})
+                   'form': form,
+                   'similar_posts': similar_posts})
 
 
 # Поделиться статьей
