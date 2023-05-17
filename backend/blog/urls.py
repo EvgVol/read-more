f39639e2bba0abb474567b0e0c6ba3ac7f9c1a84
Evgeniy V <volochek93@yandex.ru>
2023-05-17 16:34:00 +0400

    add tags by posts

--- backend/blog/urls.py
+++ backend/blog/urls.py
@@ -5,7 +5,7 @@ from . import views
 app_name = 'blog'
 
 urlpatterns = [
-    path('', views.PostListView.as_view(), name='post_list'),
+    path('', views.post_list, name='post_list'),
     path('<int:year>/<int:month>/<int:day>/<slug:post>/',
          views.post_detail,
          name='post_detail'),
@@ -13,4 +13,5 @@ urlpatterns = [
         path('share/', views.post_share, name='post_share'),
         path('comment/', views.post_comment, name='post_comment'),
     ])),
+    path('tag/<slug:tag_slug>/', views.post_list, name='post_list_by_tag')
 ]
--- backend/blog/views.py
+++ backend/blog/views.py
@@ -1,22 +1,39 @@
-from django.views.generic import ListView, DetailView, FormView
 from django.shortcuts import get_object_or_404, render
 from django.core.mail import send_mail
 from django.views.decorators.http import require_POST
+from django.core.paginator import Paginator, PageNotAnInteger, EmptyPage
 
-from .models import Post, Comment
+from taggit.models import Tag
+
+from .models import Post
 from .forms import EmailPostForm, CommentForm
 
 
-class PostListView(ListView):
-    """Представление списка постов."""
+# Отображаем список статей
+def post_list(request, tag_slug=None):
+    """Отображает список статей."""
+    post_list = Post.published.all()
+    tag = None
+    if tag_slug:
+        tag = get_object_or_404(Tag, slug=tag_slug)
+        post_list = post_list.filter(tags__in=[tag])
+    paginator = Paginator(post_list, 3)
+    page_number = request.GET.get('page', 1)
+    try:
+        posts = paginator.page(page_number)
+    except PageNotAnInteger:
+        posts = paginator.page(1)
+    except EmptyPage:
+        posts = paginator.page(paginator.num_pages)
+    return render(request, 
+                  'blog/blog.html',
+                  {'posts': posts,
+                   'tag': tag})
 
-    queryset = Post.published.all()
-    context_object_name = 'posts'
-    paginate_by = 3
-    template_name = 'blog/blog.html'
 
-
+# Отображаем детали статьи
 def post_detail(request, year, month, day, post):
+    """Отображает детали статьи."""
     post =  get_object_or_404(Post,
                               status=Post.Status.PUBLISHED,
                               slug=post,
@@ -31,7 +48,9 @@ def post_detail(request, year, month, day, post):
                    'form': form})
 
 
+# Поделиться статьей
 def post_share(request, post_id):
+    """Позволяет поделиться конкретной статьей."""
     post = get_object_or_404(Post,
                              id=post_id,
                              status=Post.Status.PUBLISHED)
@@ -55,8 +74,10 @@ def post_share(request, post_id):
                                                     'sent': sent})
 
 
+# Оставляем комментарий к статье
 @require_POST
 def post_comment(request, post_id):
+    """Оставляем комментарии к статье."""
     post = get_object_or_404(Post,
                              id= post_id,
                              status= Post.Status.PUBLISHED)
