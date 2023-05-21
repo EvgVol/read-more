93d9b3f852346da08b3b6c596c8d61000dd20b38
Evgeniy V <volochek93@yandex.ru>
2023-05-21 11:44:20 +0400

    add urls password-reset and change

--- backend/account/urls.py
+++ backend/account/urls.py
@@ -1,11 +1,34 @@
-from django.urls import path
+from django.urls import path, include
 from django.contrib.auth import views as auth_views
 
-from . import views
 
 app_name = 'account'
 
+
 urlpatterns = [
-    path('login/', auth_views.LoginView.as_view(), name='login'),
-    path('logout/', auth_views.LogoutView.as_view(), name='logout')
+    path('login/',
+         auth_views.LoginView.as_view(),
+         name='login'),
+
+    path('logout/',
+         auth_views.LogoutView.as_view(),
+         name='logout'),
+
+    path('password-change/', include([
+        path('',
+             auth_views.PasswordChangeView.as_view(),
+             name='password_change'),
+        path('done/',
+             auth_views.PasswordChangeDoneView.as_view(),
+             name='password_change_done'),
+    ])),
+
+    path('password-reset/', include([
+        path('',
+             auth_views.PasswordResetView.as_view(),
+             name='password_reset'),
+        path('done/',
+             auth_views.PasswordResetDoneView.as_view(),
+             name='password_reset_done'),
+    ])),
 ]
