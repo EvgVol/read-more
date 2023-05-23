8ecb51d09d54d527ee970de8d4ec53f0205f0147
Evgeniy V <volochek93@yandex.ru>
2023-05-23 14:30:00 +0400

    update urls.py

--- backend/account/urls.py
+++ backend/account/urls.py
@@ -6,13 +6,13 @@ from . import views
 
 urlpatterns = [
     path('signup/', views.RegisterView.as_view(), name='signup'),
-    path('login/', auth_views.LoginView.as_view(), name='login'),
-    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
-    path('password-reset/', auth_views.PasswordResetView.as_view(), name='password_reset'),
-    path('password-reset/done/', auth_views.PasswordResetDoneView.as_view(), name='password_reset_done'),
-    path('password-reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
-    path('password-reset/complete/', auth_views.PasswordResetCompleteView.as_view(), name='password_reset_complete'),
-
+    # path('login/', auth_views.LoginView.as_view(), name='login'),
+    # path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+    # path('password-reset/', auth_views.PasswordResetView.as_view(), name='password_reset'),
+    # path('password-reset/done/', auth_views.PasswordResetDoneView.as_view(), name='password_reset_done'),
+    # path('password-reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
+    # path('password-reset/complete/', auth_views.PasswordResetCompleteView.as_view(), name='password_reset_complete'),
+    path('', include('django.contrib.auth.urls')),
     path('profile/', include([
         path('<int:pk>/', views.ProfileView.as_view(), name='profile'),
         path('edit/', views.ProfileUpdateView.as_view(), name='profile_edit'),
