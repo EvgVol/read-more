7e59daf7b6747dee25ff9705b993b856218f14c4
Evgeniy V <volochek93@yandex.ru>
2023-05-31 16:51:55 +0400

    update views

--- backend/vookmarks/views.py
+++ backend/vookmarks/views.py
@@ -1,9 +1,10 @@
-from django.shortcuts import render, redirect, get_object_or_404
-from django.contrib.auth.decorators import login_required
 from django.contrib import messages
-from django.http import JsonResponse
-from django.views.decorators.http import require_POST
+from django.contrib.auth.decorators import login_required
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from django.http import JsonResponse, HttpResponse
+from django.shortcuts import render, redirect, get_object_or_404
 from django.views.decorators.csrf import csrf_exempt
+from django.views.decorators.http import require_POST
 
 from .forms import ImageCreateForm
 from .models import Image
@@ -54,4 +55,26 @@ def image_like(request):
         except Image.DoesNotExist:
             pass
     return JsonResponse({'status': 'error'})
- 
+
+
+# @login_required
+# def image_list(request):
+#     images = Image.objects.all()
+#     paginator = Paginator(images, 8)
+#     page = request.GET.get('page')
+#     images_only = request.GET.get('images_only')
+#     try:
+#         images = paginator.page(page)
+#     except PageNotAnInteger:
+#         images = paginator.page(1)
+#     except EmptyPage:
+#         if images_only:
+#             return HttpResponse('')
+
+#     # Если страница вне диапазона, то вернуть последнюю страницу результатов
+#     images = paginator.page(paginator.num_pages)
+    
+#     if images_only:
+#         return render(request, 'images/image/list_images.html', {'section': 'images', 'images': images})
+
+#     return render(request, 'images/image/list.html', {'section': 'images', 'images': images})
