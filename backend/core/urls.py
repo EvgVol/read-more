0890fe16f966a7f2987d005c01482a8ce0738425
Evgeniy V <volochek93@yandex.ru>
2023-06-02 11:04:53 +0400

    added page of dashboard

--- backend/core/urls.py
+++ backend/core/urls.py
@@ -7,6 +7,7 @@ app_name = 'core'
 
 urlpatterns = [
     path('', views.LangingPageView.as_view(), name='index'),
+    path('home/', views.HomePageView.as_view(), name='home'),
     path('setting/', views.SettingView.as_view(), name='setting'),
     path('style/', views.StyleView.as_view(), name='style'),
     path('countdown/', views.countdown_view, name='countdown_view'),
--- backend/core/views.py
+++ backend/core/views.py
@@ -1,6 +1,8 @@
 from django.views.generic import TemplateView
 from django.shortcuts import render, redirect
+from django.contrib.auth import get_user_model 
 
+from blog.models import Post, Comment
 
 class SettingView(TemplateView):
     """Отображает страницу со списком настроек."""
@@ -20,12 +22,32 @@ class LangingPageView(TemplateView):
     template_name = 'core/landing-page.html'
 
     def dispatch(self, request, *args, **kwargs):
-        """Перенаправляет авторизованного пользователя на страницу блога"""
+        """Перенаправляет авторизованного пользователя на главную страницу"""
         if request.user.is_authenticated:
-            return redirect('blog:post_list')
+            return redirect('core:home')
         else:
             return super().dispatch(request, *args, **kwargs)
 
+
+class HomePageView(TemplateView):
+    """Отображает главную страницу."""
+
+    template_name = 'core/dashboard.html'
+
+    def dispatch(self, request, *args, **kwargs):
+        """Перенаправляет неавторизованного пользователя на лендинг"""
+        if request.user.is_anonymous:
+            return redirect('core:index')
+        else:
+            return super().dispatch(request, *args, **kwargs)
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['user_count'] = get_user_model().objects.count()
+        context['post_count'] = Post.objects.count()
+        context['comment_count'] = Comment.objects.count()
+        context['like_count'] = sum(post.users_like.count() for post in Post.objects.all())
+        return context
 
 def countdown_view(request):
     return render(request, 'core/countdown.html')
