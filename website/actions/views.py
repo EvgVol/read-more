50d6ffcd244850fbd87b71626c8345487aea670c
Evgeniy V <volochek93@yandex.ru>
2023-06-05 13:32:42 +0400

    update

--- website/actions/views.py
+++ website/actions/views.py
@@ -1,16 +1,31 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 from django.http import HttpResponse
+from django.core.paginator import Paginator, PageNotAnInteger, EmptyPage
 
 from .models import Action
 
 
 @login_required
 def dashboard(request):
-    if request.user.is_superuser:
-        actions = Action.objects.all()
+    """Отображает страницу с действиями пользователя."""
+    
+    if request.user.is_staff:
+        action_list = Action.objects.all()
     else:
-        actions = Action.objects.filter(user=request.user)
-    return render(request,
-                  'actions/user_actions.html',
-                  {'actions': actions})
+        action_list = Action.objects.filter(user=request.user)
+        
+    paginator = Paginator(action_list, 10) # Показывать 10 действий на странице
+    page = request.GET.get('page')
+
+    try:
+        actions = paginator.page(page)
+    except (PageNotAnInteger, EmptyPage):
+        actions = paginator.page(1)
+    
+    actions_only = request.GET.get('actions_only')
+    
+    if actions_only:
+        return render(request, 'actions/action/detail.html', {'actions': actions})
+    
+    return render(request, 'actions/user_actions.html', {'actions': actions})
