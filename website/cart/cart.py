69b1984335fd1baeea6561b2805e5bcecf7841d1
Evgeniy V <volochek93@yandex.ru>
2023-06-14 10:03:19 +0400

    added methods of working with the cart

---
+++ website/cart/cart.py
@@ -0,0 +1,125 @@
+from decimal import Decimal
+from django.conf import settings
+
+from shop.models import Product
+
+
+class Cart:
+    """Корзина."""
+
+    def __init__(self, request):
+        """
+        Инициализировать корзину.
+        """
+        self.session = request.session
+        cart = self.session.get(settings.CART_SESSION_ID)
+
+        if not cart:
+            # сохранить пустую корзину в сеансе
+            cart = self.session[settings.CART_SESSION_ID] = {}
+        self.cart = cart
+
+    def __iter__(self):
+        """
+        Прокрутить товарные позиции корзины в цикле и
+        получить товары из базы данных.
+        """
+        product_ids = self.cart.keys()
+        # получить объекты product и добавить их в корзину
+        products = Product.objects.filter(id__in=product_ids)
+        cart = self.cart.copy()
+        for product in products:
+            cart[str(product.id)]['product'] = product
+        for item in cart.values():
+            item['price'] = Decimal(item['price'])
+            item['total_price'] = item['price'] * item['quantity']
+            yield item
+
+    def __len__(self):
+        """
+        Подсчитать все товарные позиции в корзине.
+        """
+        return sum(item['quantity'] for item in self.cart.values())
+
+    def add(self, product, quantity=1, override_quantity=False):
+        """
+        Добавить товар в корзину либо обновить его количество.
+        """
+        product_id = str(product.id)
+        if product_id not in self.cart:
+            self.cart[product_id] = {'quantity': 0, 'price': str(product.price)}
+        if override_quantity:
+            self.cart[product_id]['quantity'] = quantity
+        else:
+            self.cart[product_id]['quantity'] += quantity
+        self.save()
+
+    @property
+    def get_total_price(self):
+        """
+        Общая стоимость товаров в корзине.
+        """
+        return sum(Decimal(item['price']) * item['quantity'] for item in self.cart.values())
+
+    def get_unique_products(self):
+        """
+        Возвращает список уникальных товаров в корзине с их количеством.
+        """
+        product_ids = self.cart.keys()
+        # получить объекты product и добавить их в корзину
+        products = Product.objects.filter(id__in=product_ids)
+        cart = self.cart.copy()
+        unique_products = []
+        for product in products:
+            cart[str(product.id)]['product'] = product
+        for item in cart.values():
+            if item not in unique_products:
+                unique_products.append(item)
+        return unique_products
+
+    def increase_quantity(self, product_id):
+        """
+        Увеличивает количество товаров в корзине на 1
+        """
+        if product_id in self.items:
+            self.items[product_id]['quantity'] += 1
+
+    def decrease_quantity(self, product_id):
+        """
+        Уменьшает количество товаров в корзине на 1. Если количество товаров 
+        равно 1, то товар удаляется из корзины.
+        """
+        if product_id in self.items:
+            if self.items[product_id]['quantity'] > 1:
+                self.items[product_id]['quantity'] -= 1
+            else:
+                del self.items[product_id]
+
+    def has_product(self, product):
+        """
+        Проверить наличие товара в корзине.
+        """
+        product_id = str(product.id)
+        return product_id in self.cart
+
+    def remove(self, product):
+        """
+        Удалить товар из корзины.
+        """
+        product_id = str(product.id)
+        if product_id in self.cart:
+            del self.cart[product_id]
+            self.save()
+
+    def clear(self):
+        """
+        Очистить корзину.
+        """
+        del self.session[settings.CART_SESSION_ID]
+        self.save()
+
+
+    def save(self):
+        # пометить сеанс как "измененный",
+        # чтобы обеспечить его сохранение
+        self.session.modified = True
