505682c82d6ae8427c913f9ee0c9eba319e912cf
Evgeniy V <volochek93@yandex.ru>
2023-06-15 15:50:42 +0400

    `help_text` has been added to the Order model. Their output is configured in the template

--- website/orders/models.py
+++ website/orders/models.py
@@ -25,15 +25,21 @@ class Order (models.Model):
         get_absolute_url(): Returns the URL for the list view of all orders.
     """
     
-    first_name = models.CharField(_("first name"), max_length=50)
-    last_name = models.CharField(_("last name"), max_length=50)
-    email = models.EmailField(_("email"))
-    address = models.CharField(_("address"), max_length=250)
-    postal_code = models.CharField(_("postal code"), max_length=50)
-    city = models.CharField(_("city"), max_length=100)
-    created = models.DateTimeField(_("created"), auto_now_add=True)
-    updated = models.DateTimeField(_("updated"), auto_now=True)
-    paid = models.BooleanField(_("paid"), default=False)
+    first_name = models.CharField(_("first name"), max_length=50,
+                                  help_text=_("Enter your first name"))
+    last_name = models.CharField(_("last name"), max_length=50,
+                                 help_text=_("Enter your last name"))
+    email = models.EmailField(_("email"),
+                              help_text=_("Enter your email address"))
+    address = models.CharField(_("address"), max_length=250,
+                               help_text=_("Enter your shipping address"))
+    postal_code = models.CharField(_("postal code"), max_length=50,
+                                   help_text=_("Enter your postal code"))
+    city = models.CharField(_("city"), max_length=100,
+                            help_text=_("Enter your city"))
+    created = models.DateTimeField(_("created"), auto_now_add=True,)
+    updated = models.DateTimeField(_("updated"), auto_now=True,)
+    paid = models.BooleanField(_("paid"), default=False,)
 
     class Meta:
         ordering = ['-created']
