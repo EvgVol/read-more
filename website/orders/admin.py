286de90807c1aff2344e0b018bef888e7b5d46d3
Evgeniy V <volochek93@yandex.ru>
2023-06-16 13:39:14 +0400

    update docstring

--- website/orders/admin.py
+++ website/orders/admin.py
@@ -5,13 +5,10 @@ from .models import Order, OrderItem
 
 class OrderItemInline(admin.TabularInline):
     """
-    Inline class used to display information about items included in the order in the Django admin interface.
-    Defines the model for the inline form and adds the ability to select products by ID instead of using a dropdown.
-
-    Attributes:
-        model: The model used for the inline form.
-        raw_id_fields: Fields to display as IDs.
-
+    Inline class used to display information about items included in 
+        the order in the Django admin interface.
+    Defines the model for the inline form and adds the ability to select 
+        products by ID instead of using a dropdown.
     """
     model = OrderItem
     raw_id_fields = ['product']
@@ -21,13 +18,8 @@ class OrderItemInline(admin.TabularInline):
 class OrderAdmin(admin.ModelAdmin):
     """
     Class defining the Django admin interface for the `Order` model.
-    Defines the fields to display in the list of orders, filters, and inline elements to display items included in the order.
-
-    Attributes:
-        list_display: The fields to display in the list of orders.
-        list_filter: Filters to use in the list of orders.
-        inlines: Inline elements to display items included in the order.
-
+    Defines the fields to display in the list of orders, filters, and 
+        inline elements to display items included in the order.
     """
     list_display = ['id', 'first_name', 'last_name', 'email',
                     'address', 'postal_code', 'city', 'paid',
--- website/orders/forms.py
+++ website/orders/forms.py
@@ -4,36 +4,7 @@ from .models import Order
 
 
 class OrderCreateForm(forms.ModelForm):
-    """
-    A form for creating a new order.
-
-    ...
-    
-    Attributes
-    ----------
-    model : Order
-        A model for creating new Order instance.
-        
-    Fields
-    ------
-    first_name : CharField
-        A first name of the person placing an order.
-    last_name : CharField
-        A last name of the person placing an order.
-    email : EmailField
-        An email of the person placing an order.
-    address : CharField
-        A street address of the person placing an order.
-    postal_code : CharField
-        A postal code of the person placing an order.
-    city : CharField
-        A city where the person placing an order lives.
-        
-    Methods
-    -------
-    Meta:
-        Fields of the form.
-    """
+    """A form for creating a new order."""
     
     class Meta:
         model = Order
