8ff5424be0c5fb0077463100310dec7fd9bc2a67
Evgeniy V <volochek93@yandex.ru>
2023-06-26 11:14:59 +0400

    added order list

--- website/core/templatetags/template_tags.py
+++ website/core/templatetags/template_tags.py
@@ -14,5 +14,8 @@ def addclass(field, css):
 @register.inclusion_tag('orders/order-list.html')
 def show_orders():
     orders = Order.objects.all()
-    
-    return {'orders': orders}
+    paid_orders = Order.objects.filter(paid=True)
+    nopaid_orders = Order.objects.filter(paid=False)
+    return {'orders': orders,
+            'paid_orders': paid_orders,
+            'nopaid_orders': nopaid_orders}
