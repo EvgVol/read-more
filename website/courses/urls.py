6556fdc3f67c4cbcf28c36d6aa1e6b8e48fec517
Evgeniy V <volochek93@yandex.ru>
2023-07-06 12:49:08 +0400

    update

--- website/courses/urls.py
+++ website/courses/urls.py
@@ -11,28 +11,35 @@ app_name = 'courses'
 
 
 urlpatterns = [
+    # Курс
     path("mine/", ManageCourseListView.as_view(), name="manage_course_list"),
     path("create/", CourseCreateView.as_view(), name="course_create"),
     path("<int:pk>/", include([
         path("edit/", CourseUpdateView.as_view(), name="course_edit"),
         path("delete/", CourseDeleteView.as_view(), name="course_delete"),
-        path("module/", CourseModuleUpdateView.as_view(),name="module_update")
-    ])),
-    path('module/<int:module_id>/', include([
-        path('',
-             ModuleContentListView.as_view(),
-             name="content_list"),
-        path('content/<model_name>/', include([
-            path('create/',
-                 ContentCreateUpdateView.as_view(),
-                 name="content_create"),
-            path('<int:id>/', include([
+
+        # Модуль
+        path('module/', include([
+            path('', CourseModuleUpdateView.as_view(),name="module_update"),
+
+            # Контент
+            path('<int:module_id>/', include([
                 path('',
-                     ContentCreateUpdateView.as_view(),
-                     name='content_update'),
-                path('delete/',
-                     ContentDeleteView.as_view(),
-                     name='content_delete'),
+                    ModuleContentListView.as_view(),
+                    name='content_list'),
+                path('content/<model_name>/', include([
+                    path('create/',
+                        ContentCreateUpdateView.as_view(),
+                        name="content_create"),
+                    path('<int:id>/', include([
+                        path('',
+                            ContentCreateUpdateView.as_view(),
+                            name='content_update'),
+                        path('delete/',
+                            ContentDeleteView.as_view(),
+                            name='content_delete'),
+                    ])),
+                ])),
             ])),
         ])),
     ])),
