08574e6e2ee56fc6e2c6615e3408d81b5e6b086c
Evgeniy V <volochek93@yandex.ru>
2023-07-06 11:10:26 +0400

    update urls.py

--- website/courses/urls.py
+++ website/courses/urls.py
@@ -3,7 +3,8 @@ from django.utils.translation import gettext_lazy as _
 
 from .views import (ManageCourseListView, CourseCreateView,
                     CourseUpdateView, CourseDeleteView,
-                    CourseModuleUpdateView, ContentCreateUpdateView)
+                    CourseModuleUpdateView, ContentCreateUpdateView,
+                    ContentDeleteView, ModuleContentListView)
 
 
 app_name = 'courses'
@@ -12,15 +13,27 @@ app_name = 'courses'
 urlpatterns = [
     path("mine/", ManageCourseListView.as_view(), name="manage_course_list"),
     path("create/", CourseCreateView.as_view(), name="course_create"),
-    path("<pk>/", include([
+    path("<int:pk>/", include([
         path("edit/", CourseUpdateView.as_view(), name="course_edit"),
         path("delete/", CourseDeleteView.as_view(), name="course_delete"),
         path("module/", CourseModuleUpdateView.as_view(),name="module_update")
     ])),
-    path("module/<int:module_id>/content/<model_name>/", include([
-        path("create/", ContentCreateUpdateView.as_view(),
-             name="module_content_create"),
-        path("<id>/", ContentCreateUpdateView.as_view(),
-             name="module_content_update"),
+    path('module/<int:module_id>/', include([
+        path('',
+             ModuleContentListView.as_view(),
+             name="content_list"),
+        path('content/<model_name>/', include([
+            path('create/',
+                 ContentCreateUpdateView.as_view(),
+                 name="content_create"),
+            path('<int:id>/', include([
+                path('',
+                     ContentCreateUpdateView.as_view(),
+                     name='content_update'),
+                path('delete/',
+                     ContentDeleteView.as_view(),
+                     name='content_delete'),
+            ])),
+        ])),
     ])),
 ]
