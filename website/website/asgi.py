c57122bd73757379145ea5920d2d5ae5bbdbcfae
Evgeniy V <volochek93@yandex.ru>
2023-08-30 16:18:21 +0400

    asgi server deployed

--- website/website/asgi.py
+++ website/website/asgi.py
@@ -2,6 +2,7 @@ import os
 
 from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
+from channels.security.websocket import AllowedHostsOriginValidator
 from channels.auth import AuthMiddlewareStack
 
 import chat.routing
@@ -13,7 +14,9 @@ django_asgi_app = get_asgi_application()
 
 application = ProtocolTypeRouter({
     'http': django_asgi_app,
-    'websocket': AuthMiddlewareStack(
-        URLRouter(chat.routing.websocket_urlpatterns)
+    'websocket': AllowedHostsOriginValidator(
+        AuthMiddlewareStack(
+            URLRouter(chat.routing.websocket_urlpatterns)
+        ),
     ),
 })
